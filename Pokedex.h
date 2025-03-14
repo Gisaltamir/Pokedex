8755490db7c4f3d582171853cef7585e5b011181
Gisaltamir <140106131+Gisaltamir@users.noreply.github.com>
2025-03-14 19:29:48 +0200

    Update Pokedex.h

--- Pokedex.h
+++ Pokedex.h
@@ -1,3 +1,5 @@
+#ifndef POKEDEX_H
+#define POKEDEX_H
 #include <iostream>
 #include <vector>
 #include "Pokemon.h"
@@ -6,13 +8,20 @@ using namespace std;
 
 class Pokedex {
 private:
-    vector<Pokemon> myPokedex;
+    unordered_map<int, vector<Pokemon>> myPokedex;
 
 public:
-    Pokedex() {};
+    Pokedex::Pokedex() { cout << "Pokedex created... \n"; };
+    void selectFirstPokemon(Pokemon& pokemon);
     void capturePokemon(Pokemon p);
-    void deletePokemon(int id);
+    void deletePokemon(int id, int index);
     void showMyPokemons();
     void saveMyPokedex(const string& filename);
     void loadMyPokedex(const string& filename);
+    Pokemon* selectMyPokemon(int id, int index);
+    bool battle(Pokemon* mine, Pokemon& newPokemon);
+    bool attackPokemon(Pokemon* attacker, Pokemon& defender);
+    bool isMine(int pokeIndex, int index);
+
 };
+#endif
