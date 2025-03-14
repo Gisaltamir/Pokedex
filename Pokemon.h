5fd3b474b06eaf0c9138c8be391e56cd809c53cb
Gisaltamir <140106131+Gisaltamir@users.noreply.github.com>
2025-03-14 19:30:34 +0200

    Update Pokemon.h

--- Pokemon.h
+++ Pokemon.h
@@ -1,3 +1,5 @@
+#ifndef POKEMON_H
+#define POKEMON_H
 #include <set>
 #include <iostream>
 #include "json.hpp"
@@ -5,15 +7,37 @@
 using namespace std;
 
 class Pokemon {
-public:
+private:
     int id;
     string name;
     string type;
     string image;
+    int health;
     set<Pokemon> pokemon;
+
+public:
+    //Getters:
+    int getId() const { return id; }
+    string getName() const { return name; }
+    string getType() const { return type; }
+    string getImage() const { return image; }
+    int getHealth() const { return health; }
+
+    //Setters
+    void setId(int newId) { id = newId; }
+    void setName(string newName) { name = newName; }
+    void setType(string newType) { type = newType; }
+    void setImage(string newImage) { image = newImage; }
+    void setHealth(int newHealth) { health = max(0, newHealth); }
+
+
+
+    Pokemon() : id(0), health(100) {}
     bool operator<(const Pokemon& other) const;
-    void readJson();
+    int attack();
     void showAll();
+    void readJson();
     Pokemon searchPokemon(int id);
-    Pokemon() : id(0) {}
 };
+
+#endif
